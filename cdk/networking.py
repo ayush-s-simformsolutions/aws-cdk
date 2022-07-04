433bf226c7ff66f0b29fbf1a60fe758834ddd931
Ayush Shah <aayushshah896@gmail.com>
2022-07-04 16:46:59 +0530

    vpc with public and private subnet

---
+++ cdk/networking.py
@@ -0,0 +1,31 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_ec2 as ec2,
+)
+
+class Networking(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        vpc = ec2.Vpc(self, 'VPC', 
+            cidr = '10.0.0.0/16',
+            max_azs = 3,
+            subnet_configuration = [
+                ec2.SubnetConfiguration(
+                    cidr_mask = 24,
+                    name = 'ingress',
+                    subnet_type = ec2.SubnetType.PUBLIC,
+                ),
+                ec2.SubnetConfiguration(
+                    cidr_mask = 24,
+                    name = 'application',
+                    subnet_type = ec2.SubnetType.PRIVATE_WITH_NAT,
+                ),
+                ec2.SubnetConfiguration(
+                    cidr_mask = 24,
+                    name = 'rds',
+                    subnet_type = ec2.SubnetType.PRIVATE_ISOLATED,
+                ),
+            ]
+        )
