d920d75ec5f60d1a69bbdf2bc07fda128cad2e0b
janidevanshi <devanshi.j@simformsolutions.com>
2022-07-12 15:23:24 +0530

    added env variables

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+
 from aws_cdk import (
     Environment,
     Stack,
@@ -7,15 +9,18 @@ import aws_cdk as cdk
 import os
 from networking.networking import Networking
 from servers.servers import AutoScalingGroup
+from dotenv import load_dotenv
+
+load_dotenv()
 
 env = Environment(
-    account = os.environ["CDK_DEFAULT_ACCOUNT"], 
-    region = os.environ["CDK_DEFAULT_REGION"],
+    account = os.getenv('CDK_DEFAULT_ACCOUNT'), 
+    region = os.getenv('CDK_DEFAULT_REGION'),
 )
 app = cdk.App()
 main_stack = Stack(
-    app, 
-    "Project_name",
+    app,
+    os.getenv('Project_name'),
 )
 networking = Networking(
     main_stack, 
